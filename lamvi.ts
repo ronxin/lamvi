b354b257c354ca3ca884da664b81933fc21d1ef3
Xin Rong <ronxin@umich.edu>
2016-04-21 17:51:00 -0400

    Added basic frontend framework.

--- lamvi.ts
+++ lamvi.ts
@@ -1,13 +1,2 @@
 /// <reference path="typings/browser.d.ts" />
-
-function helloWorld() {
-  console.log("hello world");
-}
-
-helloWorld();
-helloWorld();
-
-d3.select('h1').append('h2').text('big success');
-
-d3.select('body').append('button').classed('btn', true).classed('btn-success', true).text('Click Here??');
-
+// The above reference is essential for d3 to be loaded via typings.
